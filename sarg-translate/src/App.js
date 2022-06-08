0e158bd2f2edb15ef2359ea40885343aa4c1cd69
Anubhav Gupta <mail.anubhav06@gmail.com>
2022-06-08 22:58:49 +0530

    added translations
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

--- sarg-translate/src/App.js
+++ sarg-translate/src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import LandingPage from './pages/LandingPage';
+import {TolgeeProvider} from "@tolgee/react";
 
 function App() {
   return (
     <div className="App">
       <Router>
+      <TolgeeProvider
+          staticData={{
+            en: () => import('./i18n/en.json'),
+            es: () => import('./i18n/es-ES.json'),
+            fr: () => import('./i18n/fr-FR.json'),
+          }}
+          filesUrlPrefix="i18n/"
+          apiUrl={process.env.REACT_APP_TOLGEE_API_URL}
+          apiKey={process.env.REACT_APP_TOLGEE_API_KEY}
+          loadingFallback={<>Loading...</>} //content to show when localization data is loading
+      >
         <Routes>
           <Route element={<LandingPage/>} path="/"/>
         </Routes>
+
+      </TolgeeProvider>
       </Router>
     </div>
   );
--- sarg-translate/src/pages/LandingPage.js
+++ sarg-translate/src/pages/LandingPage.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { T, useSetLanguage, useCurrentLanguage } from '@tolgee/react';
 
 
 const LandingPage = () => {
 
+    const setLanguage = useSetLanguage();
+    const getLanguage = useCurrentLanguage();
+
     return (
         <div>
-            Hi there! How are you ?
+            <h1> Multi-language React Application </h1>
+            <select
+                onChange={(e) => setLanguage(e.target.value)}
+                value={getLanguage()}
+            >
+                <option value="en"> English </option>
+                <option value="es-ES"> Spanish </option>
+                <option value="fr-FR"> French </option>
+            </select>
+            <br/><br/>
+            <T keyName="main_content">
+                This is default value.
+            </T>
         </div>       
     )
 }
